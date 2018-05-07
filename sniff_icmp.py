24f1fae37e3300231ae26725ff2419a915fc6835
zeddinarief <zeddinarief@gmail.com>
2018-05-07 17:20:45 +0700

    add

---
+++ sniff_icmp.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+
+def icmp_packet(packet):
+    if IP in packet:
+        size = packet.len    
+        if size > 50000 :
+            print("Terdapat sebuah serangan ICMP flood dari IP " + packet.getlayer(IP).src)
+
+pkts=sniff(filter="icmp", prn=icmp_packet)
--- tcp_cekport.py
+++ tcp_cekport.py
@@ -1,17 +1,18 @@
 from scapy.all import *
 
-minport = 77
-maxport = 81
+srcip = input("src ip = ")
+dstip = input("dst ip = ")
+minport = input("port minimal = ")
+maxport = input("port maksimal = ")
 list_port = []
-for numport in range(minport,maxport):
+for numport in range(int(minport),int(maxport)):
 	
-	tcp_packet = IP(src="192.168.100.7",dst="175.45.187.242")/TCP(sport=7778,dport=numport,flags="S")
-	reply = sr1(tcp_packet,timeout = 3)
+	tcp_packet = IP(src=srcip, dst=dstip)/TCP(sport=7778,dport=numport,flags="S")
+	reply = sr1(tcp_packet,timeout = 2)
 	try:
 		flag = reply.getlayer(TCP).flags
 		print(flag)
 		if(flag == "SA"):
-			#print(reply)
 			port = "port "+str(numport)+" open"
 			print("The port "+ str(numport) +" is open")
 			list_port.append(port)
